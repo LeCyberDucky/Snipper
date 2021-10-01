13b5aedef9d769898e57494d964761bec635990a
Andy <19520383+LeCyberDucky@users.noreply.github.com>
2021-10-01 15:33:36 +0200

    Reached usable state =]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+// TODO: Consider duplicate snippet tags in source files
+// TODO: Make output nicer for inactive snippets that are not overwritten
+
 #![feature(bool_to_option)]
 #![feature(format_args_capture)]
 use std::{
     collections::HashMap,
-    fs,
+    fs::{self, OpenOptions},
+    io::Write,
     path::{Path, PathBuf},
 };
 
@@ -15,6 +19,8 @@ struct Snippet {
     name: String,
     content: Option<String>,
     source_file: Option<PathBuf>,
+    comment: Option<String>,
+    active: bool,
     source: bool,
     latex: bool,
     extracted: bool,
@@ -34,19 +40,26 @@ impl PartialOrd for Snippet {
 
 impl Snippet {
     fn new(
-        begin: String,
-        content: Option<String>,
+        begin: Option<&str>,
+        content: Option<&str>,
         source_file: Option<PathBuf>,
-        end: String,
+        end: Option<&str>,
+        comment: Option<&str>,
+        active: bool,
         source: bool,
         latex: bool,
         extracted: bool,
     ) -> Result<Self> {
+        let begin = begin.context("Snippet begin tag has no name.")?;
+        let end = end.context("Snippet end tag has no name.")?;
+
         (begin == end)
             .then_some(Self {
-                name: begin.clone(),
-                content,
+                name: begin.to_owned(),
+                content: content.map(|string| string.to_owned()),
                 source_file,
+                comment: comment.map(|string| string.to_owned()),
+                active,
                 source,
                 latex,
                 extracted,
@@ -78,7 +91,13 @@ fn main() {
         .takes_value(true)
         .required(true)
         .validator(|path| Path::new(&path).is_dir().then_some(()).ok_or_else(||"Invalid LaTeX directory.".into()))
-        .help("Root directory of LaTeX document")).get_matches();
+        .help("Root directory of LaTeX document"))
+    .arg(Arg::with_name("Extract")
+        .long("Extract")
+        .takes_value(false)
+        .case_insensitive(true)
+        .help("Extract found snippets into separate snippet files for inclusion in LaTeX document"))
+        .get_matches();
 
     let source_directory = Path::new(arguments.value_of("Source").unwrap());
     let target_directory = Path::new(arguments.value_of("Target").unwrap());
@@ -89,39 +108,48 @@ fn main() {
     let tex_files = files_with_extension(latex_directory, vec!["tex".into()], false);
     let snippet_files = files_with_extension(target_directory, vec!["cpp".into()], false);
 
-    // Finding snippets tagged as follows: 
-    /* 
-    // SNIPPET:BEGIN {some_cool_snippet_name}
+    // Finding snippets tagged as follows:
+    /*
+    // SNIPPET:BEGIN {some_cool_snippet_name}${}    (the ${} part is optional - for comments/descriptions)
     ...some_cool_snippet_content...
     // SNIPPET:END {some_cool_snippet_name}
     */
-    let snippet_pattern = regex::RegexBuilder::new(
-        r"(// SNIPPET:BEGIN \{(?P<BEGIN>.*?)\}(?P<SNIPPET>.*?)// SNIPPET:END \{(?P<END>.*?)\})",
+    let snippet_pattern_active = regex::RegexBuilder::new(
+        r"(// SNIPPET:BEGIN \{(?P<BEGIN>.*?)\}(\$\{(?P<COMMENT>.*?)\})?(?P<SNIPPET>.*?)// SNIPPET:END \{(?P<END>.*?)\})",
     )
     .dot_matches_new_line(true)
     .build()
     .unwrap();
 
-    // Finding snippet inclusions that look something like this: 
+    let snippet_pattern_inactive = regex::RegexBuilder::new(
+        r"(// _SNIPPET:BEGIN \{(?P<BEGIN>.*?)\}(\$\{(?P<COMMENT>.*?)\})?(?P<SNIPPET>.*?)// _SNIPPET:END \{(?P<END>.*?)\})",
+    )
+    .dot_matches_new_line(true)
+    .build()
+    .unwrap();
+
+    // Finding snippet inclusions that look something like this:
     /*
-    \lstinputlisting{some_cool_snippet_name.cpp}
+    \lstinputlisting[...]{some_cool_snippet_name.cpp} where the [...] part is optional
     */
     let include_pattern =
-        regex::RegexBuilder::new(r"(\\lstinputlisting\{.*/(?P<SNIPPET_NAME>.*?)\.cpp.*?\})")
-            .dot_matches_new_line(true)
+        regex::RegexBuilder::new(r"(\\lstinputlisting.*?\{.*/(?P<SNIPPET_NAME>.*?)\.cpp.*?\})")
+            .dot_matches_new_line(false)
             .build()
             .unwrap();
 
     let mut snippets = HashMap::new();
-    for file in source_files {
+    for file in &source_files {
         let text = fs::read_to_string(&file).context(format!("{:?}", file));
         if let Ok(text) = text {
-            for captures in snippet_pattern.captures_iter(&text) {
+            for captures in snippet_pattern_active.captures_iter(&text) {
                 let snippet = Snippet::new(
-                    captures["BEGIN"].into(),
-                    Some(captures["SNIPPET"].into()),
+                    captures.name("BEGIN").map(|hit| hit.as_str()),
+                    captures.name("SNIPPET").map(|hit| hit.as_str()),
                     Some(file.clone()),
-                    captures["END"].into(),
+                    captures.name("END").map(|hit| hit.as_str()),
+                    captures.name("COMMENT").map(|hit| hit.as_str()),
+                    true,
                     true,
                     false,
                     false,
@@ -137,26 +165,63 @@ fn main() {
         }
     }
 
+    for file in &source_files {
+        let text = fs::read_to_string(&file).context(format!("{:#?}", file));
+        if let Ok(text) = text {
+            for captures in snippet_pattern_inactive.captures_iter(&text) {
+                let snippet = Snippet::new(
+                    captures.name("BEGIN").map(|hit| hit.as_str()),
+                    captures.name("SNIPPET").map(|hit| hit.as_str()),
+                    Some(file.clone()),
+                    captures.name("END").map(|hit| hit.as_str()),
+                    captures.name("COMMENT").map(|hit| hit.as_str()),
+                    false,
+                    true,
+                    false,
+                    false,
+                )
+                .context("Failed at creating snippet from LaTeX include statement.");
+
+                if let Ok(snippet) = snippet {
+                    snippets
+                        .entry(snippet.name.clone())
+                        .or_insert(snippet)
+                        .active = false;
+                } else {
+                    eprint!("{:#?}", snippet);
+                }
+            }
+        } else {
+            eprintln!("{:#?}", text);
+        }
+    }
+
     for file in tex_files {
         let text = fs::read_to_string(&file).context(format!("{:?}", file));
         if let Ok(text) = text {
             for captures in include_pattern.captures_iter(&text) {
-                let snippet_name = &captures["SNIPPET_NAME"];
-                snippets
-                    .entry(snippet_name.to_owned())
-                    .or_insert_with(|| {
-                        Snippet::new(
-                            snippet_name.to_owned(),
-                            None,
-                            None,
-                            snippet_name.to_owned(),
-                            false,
-                            true,
-                            false,
-                        )
-                        .expect("Failed at creating snippet from LaTeX include statement.")
-                    })
-                    .latex = true;
+                let snippet_name = captures.name("SNIPPET_NAME").map(|hit| hit.as_str());
+                let snippet = Snippet::new(
+                    snippet_name,
+                    None,
+                    None,
+                    snippet_name,
+                    None,
+                    false,
+                    false,
+                    true,
+                    false,
+                )
+                .context("Failed at creating snippet from LaTeX include statement.");
+
+                if let Ok(snippet) = snippet {
+                    snippets
+                        .entry(snippet.name.clone())
+                        .or_insert(snippet)
+                        .latex = true;
+                } else {
+                    eprintln!("{:#?}", snippet);
+                }
             }
         } else {
             eprintln!("{:#?}", text);
@@ -165,26 +230,32 @@ fn main() {
 
     for file in snippet_files {
         let snippet_name = file
-            .file_name()
+            .file_stem()
             .map(|name| name.to_str().map(|name| name.to_owned()))
             .flatten()
             .context("Unable to obtain snippet name from snippet file.");
         if let Ok(snippet_name) = snippet_name {
-            snippets
-                .entry(snippet_name.clone())
-                .or_insert_with(|| {
-                    Snippet::new(
-                        snippet_name.to_owned(),
-                        None,
-                        None,
-                        snippet_name.to_owned(),
-                        false,
-                        false,
-                        true,
-                    )
-                    .expect("Failed at creating snippet from extracted snippet file.")
-                })
-                .extracted = true;
+            let snippet = Snippet::new(
+                Some(&snippet_name),
+                None,
+                None,
+                Some(&snippet_name),
+                None,
+                false,
+                false,
+                false,
+                true,
+            )
+            .context("Failed at creating snippet from extracted snippet file.");
+
+            if let Ok(snippet) = snippet {
+                snippets
+                    .entry(snippet.name.clone())
+                    .or_insert(snippet)
+                    .extracted = true;
+            } else {
+                eprintln!("{:#?}", snippet);
+            }
         } else {
             eprintln!("{:#?}", snippet_name);
         }
@@ -197,6 +268,7 @@ fn main() {
     let mut count_width = 0;
     let mut snippet_name_width = "Snippet name:".len();
     let mut file_name_width = "Source file:".len();
+    let state_width = "Act.: ❌, Src.: ❌, TeX.: ❌, Ext.: ❌".len();
 
     for (i, snippet) in snippets.iter().enumerate() {
         count_width = format!("{}.:", i + 1).len().max(count_width);
@@ -212,30 +284,117 @@ fn main() {
     let spacer = "    ";
 
     let header = format!(
-        "{:count_width$}{spacer}{:snippet_name_width$}{spacer}  Source file:",
-        "", "Snippet name:"
+        "{:count_width$}{spacer}{:snippet_name_width$}{spacer}|{:state_width$}  | Source file:",
+        "", "Snippet name:", "State: "
     );
 
     let header_top = format!("{:1$}", " ", header.len());
     bunt::println!("{[underline]}", header_top);
-    
+
     bunt::println!("{[underline]}", header);
-    // println!("{:_<width$}", "", width = header.len());
     for (i, snippet) in snippets.iter().enumerate() {
         let file_name = snippet
             .source_file
             .as_ref()
             .and_then(|path| path.file_name().and_then(|name| name.to_str()))
             .unwrap_or("");
-        println!(
-            "{:count_width$}{spacer}{:snippet_name_width$}{spacer}| {}",
-            format!("{}.:", i + 1),
-            snippet.name,
-            file_name
+        let state = format!(
+            "Act.: {}, Src.: {}, TeX.: {}, Ext.: {}",
+            if snippet.active { "✔" } else { "❌" },
+            if snippet.source { "✔" } else { "❌" },
+            if snippet.latex { "✔" } else { "❌" },
+            if snippet.extracted { "✔" } else { "❌" }
         );
+        if snippet.source && snippet.latex && snippet.extracted {
+            println!(
+                "{:count_width$}{spacer}{:snippet_name_width$}{spacer}|{:state_width$}| {}",
+                format!("{}.:", i + 1),
+                snippet.name,
+                state,
+                file_name
+            );
+        } else {
+            bunt::println!(
+                "{2:0$}{1}{[red]4:3$}{1}|{6:5$}| {7}",
+                count_width,
+                spacer,
+                format!("{}.:", i + 1),
+                snippet_name_width,
+                snippet.name,
+                state_width,
+                state,
+                file_name
+            );
+        }
     }
 
     bunt::println!("{[underline]}", header_top);
+
+    // 2. Update snippet files
+    if !arguments.is_present("Extract") {
+        return;
+    }
+
+    for snippet in snippets {
+        if !snippet.source {
+            bunt::println!(
+                "{[red]}",
+                format!(
+                    "Snippet \"{}\" cannot be extracted. No associated source file.",
+                    snippet.name
+                )
+            );
+            continue;
+        }
+
+        let mut extraction_path = target_directory.join(&snippet.name);
+        extraction_path.set_extension("cpp");
+
+        let file = if snippet.active {
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(&extraction_path)
+                .map_err(|e| e.into())
+        } else {
+            OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(&extraction_path)
+                .context(format!("Snippet {} is inactive.", snippet.name))
+        };
+
+        if let Ok(mut file) = file {
+            if let Some(content) = snippet.content {
+                let write_status = file.write_all(content.as_bytes());
+                if write_status.is_ok() {
+                    bunt::println!(
+                        "Snippet \"{}\" extracted to file \"{:?}\".",
+                        snippet.name,
+                        extraction_path.file_name()
+                    );
+                } else {
+                    bunt::println!(
+                        "Unable to write snippet file \"{:#?}\": {:#?}",
+                        extraction_path.file_name(),
+                        write_status
+                    );
+                }
+            } else {
+                bunt::println!(
+                    "{[yellow]}",
+                    format!(
+                        "Snippet \"{}\" has no content. Empty file \"{:#?}\" created.",
+                        snippet.name,
+                        extraction_path.file_name()
+                    )
+                );
+            }
+        } else {
+            eprintln!("Unable to write snippet to file: {:#?}", file);
+            continue;
+        }
+    }
 }
 
 fn files_with_extension(
